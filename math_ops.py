fdf108dc57bb535c4f02484ecf48fa06b160061b
unknown <mmitesh698@gmail.com>
2025-12-09 12:03:28 +0530

    Add subtraction feature

--- math_ops.py
+++ math_ops.py
@@ -1,5 +1,5 @@
-def add(a, b):
-    return a + b
+def subtract(a, b):
+    return a - b
 
 if __name__ == "__main__":
-    print("Addition of 2 and 3 =", add(2, 3))
+    print("Subtraction of 5 and 2 =", subtract(5, 2))
